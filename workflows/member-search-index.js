067db8ab576c128da0b0c3a396ccc7fbdac5f9a9
alex-kumpula <alex-kumpula@users.noreply.github.com>
2025-10-24 02:11:01 +0000

    deploy: ffb79afe43875f5c0d01896737b35623adde7e35

---
+++ workflows/member-search-index.js
@@ -0,0 +1,1 @@
+var memberSearchIndex = [{"p":"com.example.syzygy_eventapp","c":"MainActivity","l":"MainActivity()","url":"com/example/syzygy_eventapp/MainActivity.html#MainActivity()"}]
---
+++ workflows/type-search-index.js
@@ -0,0 +1,1 @@
+var typeSearchIndex = [{"p":"com.example.syzygy_eventapp","l":"MainActivity","url":"com/example/syzygy_eventapp/MainActivity.html"}, {"l":"All classes","url":"allclasses.html"}]
