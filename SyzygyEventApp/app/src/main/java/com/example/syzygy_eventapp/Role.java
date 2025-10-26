93aa17452091afbdce44d83186c8eb7352f197d4
Travis Born <tborn9@gmail.com>
2025-10-26 11:58:37 -0600

    Fixed Role.java

--- SyzygyEventApp/app/src/main/java/com/example/syzygy_eventapp/Role.java
+++ SyzygyEventApp/app/src/main/java/com/example/syzygy_eventapp/Role.java
@@ -1,4 +1,21 @@
 package com.example.syzygy_eventapp;
 
-public class Role {
+/**
+ * The possible authorization roles a user can have.
+ * <p>
+ *     Each user may have one or more roles at the same time.
+ *     Roles determine the features, navigation, and privileges available to that user.
+ *     The users current active role must be one of the roles assigned to them.
+ * </p>
+ *
+ * <ul>
+ *     <li>ENTRANT - A normal user who can browse and join event waiting lists.</li>
+ *     <li>ORGANIZER - A user who can create, edit, manage events and waiting lists, and send notifications.</li>
+ *     <li>ADMIN - A privileged user who can manage all parts of the system, including removing events, users, and images.</li>
+ * </ul>
+ */
+public enum Role {
+    ENTRANT,
+    ORGANIZER,
+    ADMIN
 }
