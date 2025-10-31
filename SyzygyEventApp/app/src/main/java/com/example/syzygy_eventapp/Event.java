3f1a9c20c345c0678740982d64e09a65b8415bc7
Wren Durbano <wren.durbano@gmail.com>
2025-10-31 13:44:03 -0600

    added seperate firebase project for testing

---
+++ SyzygyEventApp/app/src/main/java/com/example/syzygy_eventapp/Event.java
@@ -0,0 +1,11 @@
+package com.example.syzygy_eventapp;
+
+import java.util.ArrayList;
+
+public class Event {
+    private String eventId;
+    private String name;
+    private ArrayList<User> waitingList;
+    private ArrayList<String> invitationIds;
+    private int capacity;
+}
