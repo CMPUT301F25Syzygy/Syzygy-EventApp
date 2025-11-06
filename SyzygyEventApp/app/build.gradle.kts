64db1b54863d5957628630c0e36beb9c9cdc19e1
Aliha <aliha@ualberta.ca>
2025-11-06 00:39:27 -0700

    done the eventListView and EventView fragments and the xmls, added an extra card view xml and idk if i should integrate findevents.xml and fragment eventlist.xml as one

--- SyzygyEventApp/app/build.gradle.kts
+++ SyzygyEventApp/app/build.gradle.kts
@@ -72,6 +72,16 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.ext.junit)
     implementation(libs.firebase.common)
+
+    // CameraX, ML, and Guava dependencies for QR Scan Fragment
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
+    implementation("androidx.camera:camera-extensions:1.3.0")
+    implementation("com.google.guava:guava:31.1-android")
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
