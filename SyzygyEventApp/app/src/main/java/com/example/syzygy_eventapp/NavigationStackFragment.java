156987383f0b5584623c4e04ba17cde6890ab3d5
Wren Durbano <wren.durbano@gmail.com>
2025-11-06 11:52:06 -0700

    made main nav menu respond to user role

--- SyzygyEventApp/app/src/main/java/com/example/syzygy_eventapp/NavigationStackFragment.java
+++ SyzygyEventApp/app/src/main/java/com/example/syzygy_eventapp/NavigationStackFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener;
