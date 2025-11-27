35105b61fb4d6fb9c59e1ab9c3f4d35c8e3694db
Alex Kumpula <alex.kumpula01@gmail.com>
2025-11-27 12:39:11 -0700

    Add buttons to the administrator layout, with a seperate fragment for each functionality

--- SyzygyEventApp/app/src/main/java/com/example/syzygy_eventapp/AdministratorFragment.java
+++ SyzygyEventApp/app/src/main/java/com/example/syzygy_eventapp/AdministratorFragment.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.firebase.firestore.ListenerRegistration;
 
@@ -22,12 +24,24 @@ import java.util.List;
  */
 public class AdministratorFragment extends Fragment {
 
-    /// User list view
-    private UserListView userListView;
-
     /// Firestore listener for user data
     private ListenerRegistration userListener;
 
+    /// Profile button
+    private LinearLayout profileButton;
+
+    /// Navigation stack fragment
+    private NavigationStackFragment navStack;
+
+    /// Required empty constructor
+    public AdministratorFragment() {
+        this.navStack = null;
+    }
+
+    /// Constructor with navigation stack
+    AdministratorFragment(NavigationStackFragment navStack) {
+        this.navStack = navStack;
+    }
 
     /**
      * Inflates the fragment layout and binds views.
@@ -36,140 +50,14 @@ public class AdministratorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_administrator, container, false);
 
-        // Bind views
-        userListView = root.findViewById(R.id.user_list_view);
+        // Bind buttons
+        profileButton = root.findViewById(R.id.profile_button);
 
-        // Set up user list click listener
-        userListView.setOnUserClickListener(user -> {
-            showUserActionDialog(user);
-        });
-
-        // Set up Firestore observers
-        setupUserObservers();
+        // Set listeners
+        profileButton.setOnClickListener(v -> navStack.pushScreen(
+                new AdminProfileListFragment(navStack)
+        ));
 
         return root;
-    }
-
-    /**
-     * Sets up Firestore listeners to populate the user lists.
-     */
-    private void setupUserObservers() {
-
-        userListener = UserController.getInstance()
-                .observeAllUsers(
-                        this::onUsersChanged,
-                        Throwable::printStackTrace
-                );
-    }
-
-    /**
-     * Called whenever Firestore sends updated user data.
-     */
-    private void onUsersChanged(List<User> users) {
-        userListView.setUsers(users);
-    }
-
-    /**
-     * Represents an action item in the user action dialog.
-     */
-    private class ActionItem {
-        String label;
-        Runnable action;
-
-        ActionItem(String label, Runnable action) {
-            this.label = label;
-            this.action = action;
-        }
-    }
-
-    /**
-     * Shows a dialog with actions for the selected user.
-     */
-    private void showUserActionDialog(User user) {
-
-        List<ActionItem> actions = new ArrayList<>();
-
-        switch (user.getRole()) {
-            case ENTRANT:
-                actions.add(new ActionItem("Promote", () ->
-                        UserController.getInstance().setUserRole(user.getUserID(), Role.ORGANIZER)
-                ));
-                actions.add(new ActionItem("Disable Picture", () ->
-                        // TODO: Disable the user's picture
-                        {}
-                ));
-                actions.add(new ActionItem("Delete", () ->
-                        showConfirmDeleteDialog(user)
-                ));
-                break;
-
-            case ORGANIZER:
-                actions.add(new ActionItem("Promote", () ->
-                        UserController.getInstance().setUserRole(user.getUserID(), Role.ADMIN)
-                ));
-                actions.add(new ActionItem("Demote", () ->
-                        UserController.getInstance().setUserRole(user.getUserID(), Role.ENTRANT)
-                ));
-                actions.add(new ActionItem("Disable Picture", () ->
-                        // TODO: Disable the user's picture
-                        {}
-                ));
-                actions.add(new ActionItem("Delete", () ->
-                        showConfirmDeleteDialog(user)
-                ));
-                break;
-
-            case ADMIN:
-                actions.add(new ActionItem("Demote", () ->
-                        UserController.getInstance().setUserRole(user.getUserID(), Role.ORGANIZER)
-                ));
-                actions.add(new ActionItem("Disable Picture", () ->
-                        // TODO: Disable the user's picture
-                        {}
-                ));
-                actions.add(new ActionItem("Delete", () ->
-                        showConfirmDeleteDialog(user)
-
-                ));
-                break;
-        }
-
-        // Convert labels to CharSequence[]
-        CharSequence[] labels = actions.stream()
-                .map(a -> a.label)
-                .toArray(CharSequence[]::new);
-
-        new AlertDialog.Builder(requireContext())
-                .setTitle(user.getName())
-                .setItems(labels, (dialog, which) -> {
-                    actions.get(which).action.run();   // elegant dispatch!
-                })
-                .show();
-    }
-
-    /**
-     * Shows a confirmation dialog before deleting a user.
-     */
-    private void showConfirmDeleteDialog(User user) {
-        new AlertDialog.Builder(requireContext())
-                .setTitle("Confirm Delete")
-                .setMessage("Are you sure you want to delete " + user.getName() + "?")
-                .setPositiveButton("Delete", (dialog, which) ->
-                        UserController.getInstance().deleteUser(user.getUserID())
-                )
-                .setNegativeButton("Cancel", null)
-                .show();
-    }
-
-    /**
-     * Cleans up Firestore listeners when the view is destroyed.
-     */
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (userListener != null) {
-            userListener.remove();
-            userListener = null;
-        }
     }
 }
